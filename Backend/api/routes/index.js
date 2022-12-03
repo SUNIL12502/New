d02b671252d884e1f6deb00886a07b65e060454d
Jay Milan Doshi <doshi.jaym@northeastern.edu>
2022-12-03 02:29:39 -0500

    Minor changes in features

--- Backend/api/routes/index.js
+++ Backend/api/routes/index.js
@@ -1,6 +1,10 @@
 import userRouter from './user-routes.js';
+import tradeRouter from './trade-router.js';
+import portfolioRouter from './portfolio-router.js';
 
 // Here we mention all the routes from our parent URL
 export default (app) => {
     app.use('/user', userRouter);
+    app.use('/trade', tradeRouter);
+    app.use('/portfolio', portfolioRouter);
 }
